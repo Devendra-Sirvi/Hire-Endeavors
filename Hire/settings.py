8ec8bc413e2e6abcd19aa69dc8db682bdffd0aea
parthpandyappp <parthpandyappp@gmail.com>
2020-09-19 19:10:46 +0530

    Deploy R-3

--- Hire/settings.py
+++ Hire/settings.py
@@ -137,19 +137,12 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = '/'
 
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = 'hireendeavors2020@gmail.com'
-#EMAIL_HOST_PASSWORD = 'HEH@2020'
-#EMAIL_PORT = '587'
-#EMAIL_USE_TLS = True 
-#EMAIL_USE_SSL = False
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'parthpandyappp@gmail.com'
-EMAIL_HOST_PASSWORD = 'jfcaxtalxiuftmfe'
+EMAIL_HOST_USER = 'hireendeavors2020@gmail.com'
+EMAIL_HOST_PASSWORD = 'uyzlqkxiyawtbcoq'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True 
 EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
