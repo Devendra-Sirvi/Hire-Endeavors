3419414305392b02f86598b01735f1c1910afb57
parthpandyappp <parthpandyappp@gmail.com>
2020-09-20 12:24:21 +0530

    Deploy R-3

--- Hire/settings.py
+++ Hire/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y5w)_tn3a_#gr^tipflkkqy*3k!k$%w2_huv%+z4ooodixwey1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =True
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
